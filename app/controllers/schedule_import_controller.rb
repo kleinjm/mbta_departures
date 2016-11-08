e9d8c0e02bd8064f14386d3a53b95c2ebd224255
James Klein <kleinjm007@gmail.com>
2016-11-07 20:22:02 -0500

    added departure deletion before fetching

--- app/controllers/schedule_import_controller.rb
+++ app/controllers/schedule_import_controller.rb
@@ -2,8 +2,8 @@ class ScheduleImportController < ApplicationController
   # GET index
   # import the latest schedule info and retirect to the departure board
   def index
-    csv_path = SCHEDULE_CSV_URI
-    CsvImporter.new(uri: csv_path).import
+    Departure.destroy_all
+    CsvImporter.new(uri: SCHEDULE_CSV_URI).import
 
     redirect_to departures_path,
                 flash: { notice: t('controller.schedule_import.success') }
