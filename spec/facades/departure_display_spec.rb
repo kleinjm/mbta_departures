de167edba61e4b8e09c17f22a9a99733a10f868f
James Klein <kleinjm007@gmail.com>
2016-11-08 23:01:32 -0500

    started testing facades

---
+++ spec/facades/departure_display_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper' # no need to load rails for PORO
+require './app/facades/departure_display'
+
+describe DepartureDisplay do
+  describe '#initialize' do
+    it 'exposes the departure, trip, and status' do
+      # TODO: Finish testing class
+    end
+  end
+end
